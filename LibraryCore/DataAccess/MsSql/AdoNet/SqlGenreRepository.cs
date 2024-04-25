4d42c5c383d1d24010d701ed497cebca1a593046
Murad <murad.yunus.2017@gmail.com>
2024-04-25 20:31:56 +0400

    Simple changes

--- LibraryCore/DataAccess/MsSql/AdoNet/SqlGenreRepository.cs
+++ LibraryCore/DataAccess/MsSql/AdoNet/SqlGenreRepository.cs
@@ -27,19 +27,75 @@ namespace LibraryCore.DataAccess.MsSql.AdoNet
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string command = $"update genres set isActive=0 where id={id}";
+
+                using (SqlCommand cmd = new SqlCommand(command, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public List<Genre> GetAll()
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection())
+            {
+                string command = $"select *from Genres where isActive=1";
+
+                using (SqlCommand cmd = new SqlCommand(command, connection))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Genre> genres = new();
+
+                        while (reader.Read())
+                        {
+                            Genre genre = new Genre()
+                            {
+                                Id = Convert.ToInt32(reader["Id"].ToString()),
+                                Name = reader["name"].ToString(),
+                                Note = reader["note"].ToString(),
+                                ModifierId = Convert.ToInt32(reader["modifierid"].ToString()),
+                                CreatedAt = Convert.ToDateTime(reader["CreatedAt"].ToString()),
+                                UpdatedAt = Convert.ToDateTime(reader["CreatedAt"].ToString()),
+                            };
+                            genres.Add(genre);
+                        }
+                        return genres;
+                    }                   
+                    
+                }
+            }
         }
+        47:37
 
         public Genre GetById(int id)
         {
-            throw new NotImplementedException();
-        }
+            using (SqlConnection connection = new SqlConnection())
+            {
+                string command = $"select *from Genres where isActive=1 and id={id}";
 
+                using (SqlCommand cmd = new SqlCommand(command, connection))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                        return null;
+
+                    return new Genre
+                    {
+                        Id = Convert.ToInt32(reader["Id"].ToString()),
+                        Name = reader["name"].ToString(),
+                        Note = reader["note"].ToString(),
+                        ModifierId = Convert.ToInt32(reader["modifierid"].ToString()),
+                        CreatedAt = Convert.ToDateTime(reader["CreatedAt"].ToString()),
+                        UpdatedAt = Convert.ToDateTime(reader["CreatedAt"].ToString()),
+                    };
+                }
+            }
+        }
         public void Update(Genre item)
         {
             throw new NotImplementedException();
